7543fc4a6f27b3d1b454f1d831cd18fbf9f7a39a
Alex Ofuya <esigbemi@gmail.com>
2021-11-16 00:36:31 +0000

    Hackers file 1

---
+++ hang.sql
@@ -0,0 +1,5 @@
+List of all the possible hacking numbers
+you must be careful about all this numbers
+take note of the owners as well. they are tricky
+echo I am the true man of the moment
+echo Having dinner at the CN tower was fun
