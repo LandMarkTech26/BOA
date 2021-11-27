80a8310ccac7077bbe5b4419d197e4a9d4aae485
Alexander Ofuya <esigbemi@gmail.com>
2021-11-27 07:51:38 +0000

    first update

--- boaconnect.java
+++ boaconnect.java
@@ -2,3 +2,5 @@ This is a java application for BOA
 we will use this to connect all servers in parallel
 this is being developed by the new developers
 I will oversee the process
+there will be 3 stages to the development of this starting in january
+phase one is for four months
