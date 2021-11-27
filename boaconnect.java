a4ff96bb254458606e93120b5a97f3dbf22fb41d
Bsparks09 <93885657+Bsparks09@users.noreply.github.com>
2021-11-27 02:59:21 -0500

    2nd update

--- boaconnect.java
+++ boaconnect.java
@@ -4,3 +4,6 @@ this is being developed by the new developers
 I will oversee the process
 there will be 3 stages to the development of this starting in january
 phase one is for four months
+we need to define the project manager
+we need to identify all the stakeholders as well
+and our different modes of communication.
